940830f91dac68f13e787ef850ddc1c2dc60333e
ayoubelmotaoukil001 <ayoubelmotaoukil001@gmail.com>
2026-02-09 12:25:47 +0100

    le model de link

---
+++ app/Models/Link.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Link extends Model
+{
+    use HasFactory ;
+
+    protected $fillable = ['title' ,'url' ,'category_id'  ,'user_id'];
+
+    public function category()
+    {
+        return $this->belongsTo(Category::class);
+    }
+    public function tags()
+    {
+        return $this->belongsToMany(Tag::class);
+    }
+
+}
