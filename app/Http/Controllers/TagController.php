e0a0964102a7fc5ef0374678fd9ba59d3f20838d
ayoubelmotaoukil001 <ayoubelmotaoukil001@gmail.com>
2026-02-09 12:24:26 +0100

    le contrelleur  de tags

---
+++ app/Http/Controllers/TagController.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\Tag;
+
+class TagController extends Controller
+{
+    public function index()
+    {
+        $tags = Tag::all();
+        return view('tags.index', compact('tags'));
+    }
+
+    public function create()
+    {
+        return view('tags.create');
+    }
+
+    public function store(Request $request)
+    {
+        $request->validate(['name' => 'required|string|max:255']);
+        Tag::create(['name' => $request->name]);
+        return redirect()->route('tags.index')->with('success', 'Tag created successfully');
+    }
+
+    public function edit(Tag $tag)
+    {
+        return view('tags.edit', compact('tag'));
+    }
+
+    public function update(Request $request, Tag $tag)
+    {
+        $request->validate(['name' => 'required|string|max:255']);
+        $tag->update(['name' => $request->name]);
+        return redirect()->route('tags.index')->with('success', 'Tag updated successfully');
+    }
+
+    public function destroy(Tag $tag)
+    {
+        $tag->delete();
+        return redirect()->route('tags.index')->with('success', 'Tag deleted successfully');
+    }
+}
