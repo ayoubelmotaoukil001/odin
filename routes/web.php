9eed69f4b3eb8ca7f17e3528a6e1b85863cb2f9f
ayoubelmotaoukil001 <ayoubelmotaoukil001@gmail.com>
2026-02-09 14:33:15 +0100

    routing syssteme updated

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,8 @@
 <?php
 
 use App\Http\Controllers\CategoryController;
+use App\Http\Controllers\LinkController;
+use App\Http\Controllers\TagController;
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
@@ -16,7 +18,26 @@ Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+
 });
+
+Route::middleware('auth')->group(function () {
+    Route::resource('categories', CategoryController::class);
+});
+
+
+
+Route::resource('links', LinkController::class);
+Route::resource('tags', TagController::class)->middleware('auth');
+
+Route::get('categories/{category}/links/create', [LinkController::class, 'create'])->name('categories.links.create');
+Route::post('categories/{category}/links', [LinkController::class, 'store'])->name('categories.links.store');
+
+Route::post('categories/{category}/links/attach', [CategoryController::class, 'attachLink'])->name('categories.links.attach');
+
+
+
+Route::post('links/{link}/tags/attach', [LinkController::class, 'attachTag'])->name('links.tags.attach');
 
 
 require __DIR__.'/auth.php';
