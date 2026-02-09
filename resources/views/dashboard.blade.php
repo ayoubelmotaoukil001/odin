a71f8a17613133ae09bc58387446738123016d23
ayoubelmotaoukil001 <ayoubelmotaoukil001@gmail.com>
2026-02-09 14:26:53 +0100

    addition de  butoon manage tags

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -1,17 +1,17 @@
 <x-app-layout>
-    <x-slot name="header">
-        <h2 class="font-semibold text-xl text-gray-800 leading-tight">
-            {{ __('Dashboard') }}
-        </h2>
-    </x-slot>
+<x-slot name="header">
+    <h2>Welcome {{ auth()->user()->name }}</h2>
+</x-slot>
 
-    <div class="py-12">
-        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
-            <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
-                <div class="p-6 text-gray-900">
-                    {{ __("You're logged in!") }}
-                </div>
-            </div>
-        </div>
-    </div>
+<div class="p-6 space-y-4">
+    <a href="{{ route('categories.index') }}" class="bg-green-500 text-white px-4 py-2 rounded">
+        Manage Categories
+    </a>
+    <a href="{{ route('links.index') }}" class="bg-black px-4 py-2 rounded text-white m-5">
+        Manage Links
+    </a>
+     <a href="{{ route('tags.index') }}" class="bg-yellow-900 px-4 py-2 rounded text-white m-5">
+        Manage tags
+    </a>
+</div>
 </x-app-layout>
