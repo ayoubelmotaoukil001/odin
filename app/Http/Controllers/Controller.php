719f63315d19bc44a7e04c42cf8801ada9bd8ce2
ayoubelmotaoukil001 <ayoubelmotaoukil001@gmail.com>
2026-02-11 17:16:34 +0100

    addition de authontiifcation

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -2,7 +2,10 @@
 
 namespace App\Http\Controllers;
 
-abstract class Controller
+use Illuminate\Foundation\Auth\Access\AuthorizesRequests; // تأكد من وجود هذا السطر
+use Illuminate\Routing\Controller as BaseController;
+
+class Controller extends BaseController
 {
-    //
+    use AuthorizesRequests; 
 }
