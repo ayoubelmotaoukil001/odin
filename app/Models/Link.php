b6b22e56080b2049b1f1dadaa64417451ce1cb8a
ayoubelmotaoukil001 <ayoubelmotaoukil001@gmail.com>
2026-02-23 08:02:37 +0000

    feat (Link.php) edited Added favoriteLinks relationship and isFavoritedBy helper method to support the bookmarking system.

--- app/Models/Link.php
+++ app/Models/Link.php
@@ -12,7 +12,6 @@ class Link extends Model
     use HasFactory ;
 
     protected $fillable = ['title' ,'url' ,'category_id'  ,'user_id'];
-
     public function category()
     {
         return $this->belongsTo(Category::class);
@@ -20,6 +19,38 @@ class Link extends Model
     public function tags()
     {
         return $this->belongsToMany(Tag::class);
+    }
+
+    
+    public function favoritedBy()
+    {
+        return $this->belongsToMany(User::class, 'favorites')->withTimestamps();
+    }
+
+
+    public function sharedWith()
+    {
+        return $this->belongsToMany(User::class, 'link_user')
+                    ->withPivot('permission')
+                    ->withTimestamps();
+    }
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+    public function sharedWithUsers()
+    {
+        return $this->belongsToMany(User::class, 'link_user')
+                    ->withPivot('permession') 
+                    ->withTimestamps();
+    }
+
+//**unsderstaand this */
+    public function isFavoritedBy($user): bool
+    {
+        if (!$user) return false;
+        return $this->belongsToMany(User::class, 'favorites')->where('user_id', $user->id)->exists();
     }
 
 }
